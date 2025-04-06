d78ac10e619a5c8ba9fe4fb94d9a94c3fece7979
Matt Koski <maccam912@gmail.com>
2025-04-06 07:14:51 -0500

    update: increase num_env_runners and worker replicas to 32 for improved performance

--- marl_script.py
+++ marl_script.py
@@ -7,7 +7,7 @@ config = (
     PPOConfig()
     .environment("Taxi-v3")
     .env_runners(
-        num_env_runners=2,
+        num_env_runners=32,
         # Observations are discrete (ints) -> We need to flatten (one-hot) them.
         env_to_module_connector=lambda env: FlattenObservations(),
     )
